65af00194e6211bde31abac81c338aba3007b988
PeterYangs <904801074@qq.com>
2018-09-05 09:52:14 +0800

    第一版

---
+++ src/RedisMQ.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/5 0005
+ * Time: 09:29
+ */
+namespace peteryang\src;
+
+class RedisMQ
+{
+
+
+    function __construct()
+    {
+
+        echo 123;
+    }
+
+}
